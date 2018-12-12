2277b0b36bb1262ea9a134343c830a28aa95749f
AoyamaNaoki <45162737+AoyamaNaoki@users.noreply.github.com>
2018-12-12 13:36:30 +0900

    urlの変更

--- src/main/java/sample/ExecController.java
+++ src/main/java/sample/ExecController.java
@@ -5,20 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController // (1)
 public class ExecController {
-	@RequestMapping("/") // URLが/helloのときにhelloメソッドを呼ぶ
+	@RequestMapping("/1") // URLが/helloのときにhelloメソッドを呼ぶ
 	public String Exec1() {
 		String s;
 		s = Run.Exec1();
 		return s;
 	}
 
-	@RequestMapping("/1")
+	@RequestMapping("/2")
 	public String Exec2() {
 		String s = Run.Exec2();
 		return s;
 	}
 
-	@RequestMapping("/2")
+	@RequestMapping("/3")
 	public String Exec3() {
 		String s = Run.Exec3();
 		return s;
